0cf15c3853ca139ef2a3dcd04e641c515a892fbc
infxnnite8 <dorianquintana@gmail.com>
2023-10-23 09:28:39 -0700

    Form data is getting through!

--- controllers/places.js
+++ controllers/places.js
@@ -4,18 +4,19 @@ const router = require('express').Router()
 // GET /places
 router.get('/', (req, res) => {
     let places = [{
-        name: 'H-Thai-ML',
-        city: 'Las Vegas',
-        state: 'TX',
-        cuisines: 'Thai, Pan-Asian',
-        pic: 'http://placekitten.com/250/250'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Las Vegas',
-        state: 'NV',
-        cuisines: 'Coffee, Bakery',
-        pic: 'http://placekitten.com/250/250'
-      }]
+  name: 'H-Thai-ML',
+  city: 'Seattle',
+  state: 'WA',
+  cuisines: 'Thai, Pan-Asian',
+  pic: 'http://placekitten.com/250/250'
+}, {
+  name: 'Coding Cat Cafe',
+  city: 'Phoenix',
+  state: 'AZ',
+  cuisines: 'Coffee, Bakery',
+  pic: 'http://placekitten.com/250/250'
+}]
+
       
     res.render('places/index', { places })
 })
