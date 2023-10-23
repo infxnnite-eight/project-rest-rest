f0ad02a818eeb7bc760c0ef304493a64798a4208
infxnnite8 <dorianquintana@gmail.com>
2023-10-23 14:21:11 -0700

    navbar

--- controllers/places.js
+++ controllers/places.js
@@ -30,5 +30,17 @@ router.get('/new', (req, res) => {
   res.send('POST /places')
 })
 
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  } else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+      places.splice(id, 1)
+      res.redirect('/places')
+  }
+}) 
 
 module.exports = router
