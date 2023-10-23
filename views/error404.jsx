ff950fc9fc1746f132cc3fac7cf5f7957a328142
infxnnite8 <dorianquintana@gmail.com>
2023-10-23 15:47:56 -0700

    part-seven-bug

--- views/error404.jsx
+++ views/error404.jsx
@@ -6,7 +6,7 @@ function error404 () {
       <Def>
           <main>
               <h1>404: PAGE NOT FOUND</h1>
-              <p>Oops, sorry, we can't find this page!</p>
+              <p>Oops, sorry, we can't find this page</p>
           </main>
       </Def>
     )
